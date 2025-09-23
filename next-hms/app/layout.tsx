43ac69eff287f25d4f7b1eb39a011900da045e52
Nkwi Cyril <akinimbomnkwi@gmail.com>
2025-09-23 08:55:19 +0100

    Project setup; tailwindcss@v3, shadcn/ui

---
+++ next-hms/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "./lib/utils";
+
+const jakarta_sans = Plus_Jakarta_Sans({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-sans",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "HealthLine",
+  description:
+    "A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-dark-300 font-sans antialiased",
+          jakarta_sans.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
