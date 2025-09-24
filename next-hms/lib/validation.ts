a465d887d543d7bd55123a1f50c1dbcd36b60f5c
Nkwi Cyril <akinimbomnkwi@gmail.com>
2025-09-24 03:30:53 +0100

    Including zod form validation schema for registration

---
+++ next-hms/lib/validation.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+
+export const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Full name must be at least 2 characters.",
+  }),
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+  phone_number: z.string().min(10, {
+    message: "Please enter a valid phone number.",
+  }),
+});
